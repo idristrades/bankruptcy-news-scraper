9929cb7b532e3a3bf61dd5ded86806b7b117de26
Prince Idris <gualdjouma@aim.com>
2022-07-09 22:12:12 -0400

    Add files via upload

---
+++ bankruptcy.py
@@ -0,0 +1,19 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+query = "insolvency OR bankruptcy since:2022-07-01   from:newsfilterio OR from:WSJbankruptcy"
+tweets = []
+limit = 7
+
+
+for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+
+    if len(tweets) == limit:
+        break
+    else:
+        tweets.append([tweet.date,tweet.user.username, tweet.content, tweet.tcooutlinks])
+        
+df = pd.DataFrame(tweets, columns=['Date', 'Username', 'Tweet', 'Url'])
+print(df)
+
+#If you want to save the data, you could use df.to_csv('tweets.csv')
